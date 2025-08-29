6ec78f1ba893f42eea549cf6c0d5cc2f73ba1fec
haemin4738 <leeheamin12@gmail.com>
2025-08-29 10:40:46 +0900

    t15 -g

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -64,4 +64,8 @@ public class PostService {
 
         return postRepository.deleteByIds(ids);
     }
+
+    public Post findByIdWithAuthorName(int id) {
+        return postRepository.findByIdWithAuthorName(id);
+    }
 }
