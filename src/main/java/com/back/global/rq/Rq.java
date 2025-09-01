4123859d3094655d8b9e98c312501785e030346d
haemin4738 <leeheamin12@gmail.com>
2025-09-01 11:19:26 +0900

    Session Scope Login

---
+++ src/main/java/com/back/global/rq/Rq.java
@@ -0,0 +1,21 @@
+package com.back.global.rq;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class Rq {
+    @Getter
+    private int count;
+
+    @Getter
+    @Setter
+    private String name;
+
+    public int increaseCount() {
+        return count++;
+    }
+}
